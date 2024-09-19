8cbee7a160ad2ad3b9c574d7ae0fab792878b790
Nooruz <abdykalykovnooruz638@gmail.com>
2024-09-19 17:20:31 +0300

    remove header

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Header.css";
-export const Header = ({ children }) => {
-  return <header className="header-two">{children}</header>;
-};
-
-export default Header;
