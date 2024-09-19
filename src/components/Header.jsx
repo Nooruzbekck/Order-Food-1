db55f28a5ca3419dada5223c8ba221fd2264f16b
gulnur <abdrashitovagulnur05@gmail.com>
2024-09-19 16:10:49 +0300

    feature/Button/Add

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Header.css";
+export const Header = ({ children }) => {
+  return <header className="header-two">{children}</header>;
+};
+
+export default Header;
